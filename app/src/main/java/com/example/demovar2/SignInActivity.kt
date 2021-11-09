12d3199ba225c5379c6d9277dabc2ca62a220287
ArcKontyR <kontyrtime@mail.ru>
2021-11-09 11:58:46 +0300

    qeq

--- app/src/main/java/com/example/demovar2/SignInActivity.kt
+++ app/src/main/java/com/example/demovar2/SignInActivity.kt
@@ -5,9 +5,8 @@ import android.os.Bundle
 import android.app.AlertDialog
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.widget.EditText
+import java.util.regex.Pattern.compile
 
 class SignInActivity : AppCompatActivity() {
     lateinit var email: EditText
@@ -18,29 +17,27 @@ class SignInActivity : AppCompatActivity() {
         email = findViewById(R.id.email)
         password = findViewById(R.id.password)
     }
+    val pattern =("[a-z0-9]{1,256}" +
+            "\\@" +
+            "[a-z]{1,10}" +
+            "\\." +
+            "[a-z]{1,3}")
+    fun EmailValid (email:String):Boolean{
+        return compile (pattern).matcher(email).matches()}
 
-    fun signin(view: android.view.View) {
+    fun siasignin(view: android.view.View) {
         if(email.text.toString().isNotEmpty() && password.text.toString().isNotEmpty()){
-            val log = MyRetrofit().getRetrofit()
-            val getApi = log.create(Interface::class.java)
-            val hashMap: HashMap<String,String> = HashMap<String,String>()
-            hashMap.put("email",email.text.toString())
-            hashMap.put("password",password.text.toString())
-            val log_call:Call<Void> = getApi.getAuth(hashMap)
-            log_call.enqueue(object:retrofit2.Callback<Void>{
-                override fun onResponse(call: Call<Void>, response: Response<Void>) {
-                    if (response.isSuccessful)
-                    {
-                        val intent = Intent(this@SignInActivity,MenuActivity::class.java)
-                        startActivity(intent)
-                    }
-                }
-
-                override fun onFailure(call: Call<Void>, t: Throwable) {
-                    Toast.makeText(this@SignInActivity, t.message, Toast.LENGTH_SHORT).show()
-                }
-            })
-            Intent(this, MenuActivity::class.java)
+            if (EmailValid(email.text.toString())) {
+                val intent = Intent(this@SignInActivity, MainActivity::class.java)
+                startActivity(intent)
+            } else{
+                val alert = AlertDialog.Builder(this)
+                .setTitle("Ошибка")
+                .setMessage("Email введен неверно")
+                .setPositiveButton("OK", null)
+                .create()
+                .show()
+            }
         }
         else{
             val alert = AlertDialog.Builder(this)
@@ -51,9 +48,8 @@ class SignInActivity : AppCompatActivity() {
                 .show()
         }
     }
-    fun signup(view: android.view.View) {
+    fun siasignup(view: android.view.View) {
         val intent = Intent(this@SignInActivity,SignUpActivity::class.java)
         startActivity(intent)
         }
     }
-}
--- app/src/main/java/com/example/demovar2/SignUpActivity.kt
+++ app/src/main/java/com/example/demovar2/SignUpActivity.kt
@@ -2,10 +2,77 @@ package com.example.demovar2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.CountDownTimer
+import android.widget.EditText
+import kotlin.concurrent.timer
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var email: EditText
+    lateinit var password: EditText
+    lateinit var repeatpassword: EditText
+    lateinit var name: EditText
+    lateinit var surname: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        repeatpassword = findViewById(R.id.repeatpassword)
+        name = findViewById(R.id.name)
+        surname =findViewById(R.id.surname)
     }
+
+    fun suasignup(view: android.view.View){
+        if (email.text.toString().isNotEmpty()
+            && password.text.toString().isNotEmpty()
+            && repeatpassword.text.toString().isNotEmpty()
+            && password.text.toString() == repeatpassword.text.toString()
+            && name.text.toString().isNotEmpty()
+            && surname.text.toString().isNotEmpty()){
+            val alert = AlertDialog.Builder(this)
+                .setTitle("Уведомление")
+                .setMessage("Вы зарегистрировались")
+                .setPositiveButton("OK", null)
+                .create()
+                .show()
+            val timer = object: CountDownTimer(1500,250){
+                override fun onTick(p0: Long) {
+
+                }
+
+                override fun onFinish(){
+                    val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            timer.start()
+        } else
+        {
+            if(password.text.toString() != repeatpassword.text.toString()){
+            val alert = AlertDialog.Builder(this)
+            .setTitle("Ошибка")
+            .setMessage("Пароли не совпадают")
+            .setPositiveButton("OK", null)
+            .create()
+            .show()
+            } else
+            {
+                val alert = AlertDialog.Builder(this)
+                    .setTitle("Ошибка")
+                    .setMessage("Заполните все поля")
+                    .setPositiveButton("OK", null)
+                    .create()
+                    .show()
+            }
+        }
+    }
+    fun suasignin(view: android.view.View){
+            val intent = Intent(this@SignUpActivity,SignInActivity::class.java)
+            startActivity(intent)
+        }
 }
